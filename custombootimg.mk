ff8b3fa376777ae9d827f62c34a82668300e32a5
freexperia <jerpelea@gmail.com>
2012-07-02 21:15:46 +0300

    fuji-common: sync with 6.1 release

--- custombootimg.mk
+++ custombootimg.mk
@@ -9,14 +9,14 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(recovery_ramdisk) $(I
 	$(hide) cp -R $(PRODUCT_OUT)/recovery/root/sbin/* $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
 	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
-	$(hide) python $(MKELF) -o $@ $(TARGET_PREBUILT_KERNEL)@0x40208000 $(PRODUCT_OUT)/combinedroot.fs@0x41200000,ramdisk vendor/sony/nozomi/proprietary/etc/firmware/RPM.bin@0x20000,rpm
+	$(hide) python $(MKELF) -o $@ $(TARGET_PREBUILT_KERNEL)@0x40208000 $(PRODUCT_OUT)/combinedroot.fs@0x41300000,ramdisk vendor/sony/nozomi/proprietary/etc/firmware/RPM.bin@0x20000,rpm
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 	$(recovery_ramdisk) \
 	$(recovery_kernel)
 	@echo ----- Making recovery image ------
-	$(hide) python $(MKELF) -o $@ $(TARGET_PREBUILT_KERNEL)@0x40208000 $(PRODUCT_OUT)/ramdisk-recovery.img@0x41200000,ramdisk vendor/sony/nozomi/proprietary/etc/firmware/RPM.bin@0x20000,rpm
+	$(hide) python $(MKELF) -o $@ $(TARGET_PREBUILT_KERNEL)@0x40208000 $(PRODUCT_OUT)/ramdisk-recovery.img@0x41300000,ramdisk vendor/sony/nozomi/proprietary/etc/firmware/RPM.bin@0x20000,rpm
 	@echo ----- Made recovery image -------- $@
 #	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 
