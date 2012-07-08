de9222fb81c5a73850c32f60353fcfcf9a243ca3
Giulio Cervera <giulio.cervera@gmail.com>
2012-07-08 15:24:50 +0200

    fuji-common: update wifi name

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,7 +44,7 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P          := "/vendor/firmware/fw_bcmdhd_p2p.bin"
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcm4330/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
 
 # Audio
 COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
