f33361ff537e61d1bcab5c4eb1e3cdc4c9f9bc03
Bin4ry <andreas.makris@googlemail.com>
2012-07-25 15:00:24 +0200

    Fix Blue Delay-File

--- liblights/lights.h
+++ liblights/lights.h
@@ -8,11 +8,11 @@ char const*const BLUE_LED_FILE_TRIGGER	= "/sys/class/leds/blue/trigger";
 
 char const*const RED_LED_FILE_DELAYON	= "/sys/class/leds/red/delay_on";
 char const*const GREEN_LED_FILE_DELAYON	= "/sys/class/leds/green/delay_on";
-char const*const BLUE_LED_FILE_DELAYON	= "/sys/class/leds/green/delay_on";
+char const*const BLUE_LED_FILE_DELAYON	= "/sys/class/leds/blue/delay_on";
 
 char const*const RED_LED_FILE_DELAYOFF	= "/sys/class/leds/red/delay_off";
 char const*const GREEN_LED_FILE_DELAYOFF	= "/sys/class/leds/green/delay_off";
-char const*const BLUE_LED_FILE_DELAYOFF	= "/sys/class/leds/green/delay_off";
+char const*const BLUE_LED_FILE_DELAYOFF	= "/sys/class/leds/blue/delay_off";
 
 
 char const*const BUTTON_BACKLIGHT_FILE[] = {
