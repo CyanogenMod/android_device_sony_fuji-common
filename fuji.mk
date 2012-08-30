bdc50b85685ee37c587556693e55518a6fe9e4c2
Giulio Cervera <giulio.cervera@gmail.com>
2012-08-30 06:55:27 +0200

    fuji-common: remove local sip permission
    already included by cm phone
    
    Change-Id: Ic34560d4d590acff35354fb9eb322b643613da9f

--- fuji.mk
+++ fuji.mk
@@ -19,7 +19,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
